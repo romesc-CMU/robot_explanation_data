b1c0651fe0efd0815f3e0f12d258dd430bd90f77
Rosario Scalise <rscalise@andrew.cmu.edu>
2016-09-26 19:36:12 -0400

    plotting

--- parseJSON.py
+++ parseJSON.py
@@ -3,6 +3,66 @@ from collections import OrderedDict
 from collections import defaultdict
 import ipdb
 
+# Generate a visualization
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+_ROTATION_DEGREES = 40 
+_BOTTOM_MARGIN = 0.35
+_COLOR_THEME = 'Blues_d'
+_COLOR_RES = 10 
+_LABEL_X = "Frequency"
+_LABEL_Y = "Words"
+_TITLE = "Cost Function 1"
+_MAX_LABEL_LENGTH = 20
+
+def plot_chart(data):
+    labels_raw = []
+    values_raw = []
+    for (word, freq) in data:
+        labels_raw.append(word)
+        values_raw.append(freq)
+
+    labels = np.array(labels_raw)
+    values = np.array(values_raw)
+
+    # Define color pallete (if not using default theme)
+    #===== Case switching (usefuly for highlight colors) 
+    #pal = ['gray' if (x < .3*max(values)) else 'pink' if (x < .6*max(values)) else 'red' for x in values ] 
+    #===== Value scaling (scales across a particular default palette)
+    defPal = sns.color_palette(_COLOR_THEME, _COLOR_RES) 
+    inds = [int(v) for v in values*_COLOR_RES/max(values)]
+    inds = [_COLOR_RES - i for i in inds] 
+    pal = [defPal[i] for i in inds]        
+
+    # Create a plot
+    sns.set(style="white", context="talk")
+    (f, ax) = plt.subplots(1)
+
+    bar_chart = sns.barplot(
+        x=values,
+        y=labels,
+        palette=pal
+        )
+
+    # Set labels
+    ax.set_title(_TITLE)
+    ax.set_xlabel(_LABEL_X)
+    ax.set_ylabel(_LABEL_Y)
+
+    # Rotate the x-labels
+    #bar_chart.set_xticklabels(labels, rotation=_ROTATION_DEGREES)
+
+    # Add some margin to the borrom so the labels arent cutoff
+    #plt.subplots_adjust(bottom=_BOTTOM_MARGIN)
+
+    # Stylistic
+    # remove borders
+    plt.setp(ax.patches, linewidth=0)
+    sns.despine(f,ax,top=True,right=True,left=True,bottom=True)
+
+############ IMPORT DATA AND START PROCESSING ##############
 with open("1.json") as raw:
     json_obj = json.load(raw, object_pairs_hook=OrderedDict); 
 
@@ -33,10 +93,37 @@ for session in complete_list:
 # Sort descriptions into dict accessable by cost function 
 desc_show_dict = defaultdict(list) 
 for desc_s in descs_show:
-    desc_show_dict[desc_s[1]].append(desc_s[0])
+    desc_show_dict[desc_s[1]].append(desc_s[0].decode("utf-8"))
 
 desc_ask_dict = defaultdict(list) 
 for desc_a in descs_ask:
-    desc_ask_dict[desc_a[1]].append(desc_a[0])
+    desc_ask_dict[desc_a[1]].append(desc_a[0].decode("utf-8"))
+
+
+# NLP related stuff from here on down
+costfunIdx_toLoad = '1'
+import spacy
+from collections import defaultdict, Counter
+nlp = spacy.load('en')
+# create doc by joining each sentence with white space
+doc = nlp(' '.join(desc_show_dict[costfunIdx_toLoad]))
+
+pos_counts = defaultdict(Counter)
+for token in doc:
+    pos_counts[token.pos][token.orth] += 1
+
+for pos_id, counts in sorted(pos_counts.items()):
+    pos = doc.vocab.strings[pos_id]
+    data = []
+    for orth_id, count in counts.most_common():
+        print(pos, count, doc.vocab.strings[orth_id])
+        data.append( (doc.vocab.strings[orth_id], count) )
+    plot_chart(data)    
+    sns.plt.show()
+    ipdb.set_trace()
+
+
+
+
 
 ipdb.set_trace()
